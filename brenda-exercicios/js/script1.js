72dacfc7c2190093ddc4c61ec1e00cafdc9413b7
Brenda Melo <brendasilvam7@gmail.com>
2026-06-09 17:23:15 -0300

    exercicio-aula04

---
+++ brenda-exercicios/js/script1.js
@@ -0,0 +1,64 @@
+/*## 1. Maior entre dois números
+
+let numero1= Number(prompt("Informe um número"))
+let numero2= Number(prompt("Informe mais um número"))
+if(numero1 > numero2){
+    alert("Número 1 informado é maior que número 2")
+}else{
+    alert("Número 2 informado é maior que número 1")
+}*/
+
+/*## 2. Número positivo ou negativo
+
+let numero= Number(prompt("Informe um número"))
+let resultado= numero >0? "Positivo": "Negativo"
+alert(resultado)
+*/
+
+/*## 3. Identificação de sexo
+let sexo = prompt("Informe seu gênero, F para feminino, M para masculino").toLowerCase()
+switch (sexo) {
+    case "f": alert("Feminino")
+        break
+    case "m": alert("Masculino")
+        break
+    default: alert("Sexo inválido")
+}*/
+
+/*## 4. Vogal ou consoante
+
+let letra= prompt("Informe uma letra").toLowerCase()
+switch(letra){
+    case "a": alert("Vogal")
+    break
+    case "e": alert("Vogal")
+    break
+    case "i": alert("Vogal")
+    break
+    case "o": alert("Vogal")
+    break
+    case "u": alert("Vogal")
+    break
+    default: alert("Consoante")
+}*/
+
+/*## 5. Média de notas
+
+let nota= Number(prompt("Informe a nota do aluno"))
+if(nota >=10){
+    alert("Aprovado com Distinção!")
+}else if(nota >=7){
+    alert("Aprovado")
+}else{
+    alert("Reprovado")
+}
+*/
+
+/*## 6. Maior de três números*/
+let numero1= Number(prompt("Informe primeiro número"))
+let numero2= Number(prompt("Informe segundo um número"))
+let numero3= Number(prompt("Informe o terceiro Número"))
+if(numero1 > numero2){
+    alert("Número 1 informado é maior que número 2")
+}else{
+    alert("Número 2 informado é maior que número 1")
