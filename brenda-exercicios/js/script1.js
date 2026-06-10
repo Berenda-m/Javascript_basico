553ed44b37561b2d2664dc94361645ca327a67d9
Brenda Melo <brendasilvam7@gmail.com>
2026-06-10 17:25:15 -0300

    novas-respostas

--- brenda-exercicios/js/script1.js
+++ brenda-exercicios/js/script1.js
@@ -54,11 +54,49 @@ if(nota >=10){
 }
 */
 
-/*## 6. Maior de três números*/
+/*## 6. Maior de três números
 let numero1= Number(prompt("Informe primeiro número"))
 let numero2= Number(prompt("Informe segundo um número"))
 let numero3= Number(prompt("Informe o terceiro Número"))
-if(numero1 > numero2){
-    alert("Número 1 informado é maior que número 2")
+if (numero1 > numero2 && numero1 > numero3) {
+    alert("Número 1 informado é o maior")
+} else if (numero2 > numero1 && numero2 > numero3) {
+    alert("Número 2 informado é o maior")
+} else {
+    alert("Número 3 informado é o maior")
+}*/
+
+/*## 7. Maior e menor de três números
+let numero1= Number(prompt("Informe primeiro número"))
+let numero2= Number(prompt("Informe segundo um número"))
+let numero3= Number(prompt("Informe o terceiro Número"))
+if(numero1 > numero2 && numero1 > numero3){
+    alert(" O maior número digitado foi " + numero1)
+}else if(numero2 > numero1 && numero2 > numero3){
+    alert("O maior número digitado foi " + numero2)
+}else if(numero3 > numero1 && numero3> numero2){
+    alert("O maior número digitado foi " + numero3)
 }else{
-    alert("Número 2 informado é maior que número 1")
+    alert("Resposta Inválida")
+}
+if(numero1 < numero2 && numero1 < numero3){
+    alert("O menor número digitado foi " + numero1)
+}else if (numero2 < numero1 && numero2 < numero3){
+    alert("O menor número digitado foi " + numero2)
+}else if(numero3 < numero1 && numero3 < numero2){
+    alert("O menor número digitado foi " + numero3)
+}else{
+    alert("Resposta Inválida")
+}*/
+
+
+/*## 8. Produto mais barato}*/
+let produto1= Number(prompt("Informe o valor do primeiro produto"))
+let produto2= Number(prompt("Informe valor do segundo produto"))
+let produto3= Number(prompt("Informe o valor do terceiro produto"))
+let maisBarato= 
+if(produto1 < produto2 && produto1 < produto3){    
+}else if(produto2 < produto1 && produto2 < produto3){    
+}else if (produto3 < produto1 && produto3 < produto2){
+}else{
+    alert("O produto de menor valor")
