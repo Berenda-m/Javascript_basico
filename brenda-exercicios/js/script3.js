1c0e6d03ff9c46bb6df41842899a31ed4e699912
Brenda Melo <brendasilvam7@gmail.com>
2026-06-18 17:21:06 -0300

    exercicios-aula-05

---
+++ brenda-exercicios/js/script3.js
@@ -0,0 +1,85 @@
+
+/*for (let i = 1; i <= 10; i++){
+    alert(i)
+}*/
+
+/*for (let i = 1; i <= 20; i++)
+    if(i % 2 == 0){
+    console.log("Número par: " +i)
+}else{
+}*/
+
+/*for (let i = 5; i >= 0; i--) {
+    alert(i)
+} alert("Lançamento da SpaceX!! 🚀✨")*/
+
+/*let numero = 2
+
+for (let i = 1; i <= 10; i++) {
+    console.log(numero + "x" + i + " = " + (numero * i));
+}*/
+
+/*let bateria = 100;
+
+while(bateria > 0){
+    alert("Bateria: " + bateria + "%")
+    bateria -= 20;
+} alert("Adeus")*/
+
+/*let valor = 500;
+
+while (valor > 0) {
+    alert("Seu dinheiro está acabando restam R$ " + valor )
+    valor -= 150;
+}*/
+
+/*let senha = "";
+
+while (senha !== "123"){
+    senha = prompt("Digite sua senha");
+    if(senha != "123"){
+        alert("Senha incorreta")
+    }
+} alert("Acesso liberado!")*/
+
+/*let energia = 100;
+
+while(energia > 0){
+    alert("A energia está em: " + energia + "%")
+    energia -= 10;
+} alert("A energia acabou")*/
+
+/*
+let opcao
+
+do {
+    opcao = Number(prompt("Digite a opção desejada, 1- para jogar, 2- para Ranking, 3- para Sair"))
+    switch (opcao) {
+        case 1: alert("Jogar")
+            break
+        case 2: alert("Ranking")
+            break
+        case 3: alert("Sair")
+            break
+        default: alert("Opção inválida")
+    }
+} while (opcao !== 3) {
+}*/
+
+/*let frutas =[
+    "maçã",
+    "uva",
+    "banana",
+]
+console.log(frutas[0])*/
+
+let playlist= [
+    "The Strokes - Reptilia 🕺",
+    "Gal Costa - Barato total 🕺",
+    "Pink Floyd - Breathe 🕺"
+]
+for(let i = 0; i < playlist.length; i++){
+    console.log(playlist[2])
+}
+
+
